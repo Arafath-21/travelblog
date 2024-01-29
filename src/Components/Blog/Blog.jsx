b5562d4549d204e11232fb345df5987c58bef684
arafathyas21@gmail.com <arafathyas21@gmail.com>
2024-01-29 23:27:50 +0530

    v1.1

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import manInlaptop from '../../assets/manInlaptop.png'
+import manInlaptop from '../../assets/manInlaptop.png';
 
 function Blog()  {
 
