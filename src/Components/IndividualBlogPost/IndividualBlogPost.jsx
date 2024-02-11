adbf7f0809db5a6ff13d0d2d258049f295ca8e90
arafathyas21@gmail.com <arafathyas21@gmail.com>
2024-02-12 03:19:08 +0530

    finalVersion v3.0

--- src/Components/IndividualBlogPost/IndividualBlogPost.jsx
+++ src/Components/IndividualBlogPost/IndividualBlogPost.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate ,useParams } from "react-router-dom";
 import axios from 'axios';
-import toast, { Toaster } from 'react-hot-toast';
 
 function IndividualBlogPost() {
   const navigate = useNavigate();
